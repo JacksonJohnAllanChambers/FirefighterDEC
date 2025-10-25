7398e0b4e7daf49b686f38a422fbab0a90a1bcd8
ryan6625 <rmleblanc05@gmail.com>
2025-10-25 12:56:16 -0300

    updated fireprediction

--- FirePrediction.py
+++ FirePrediction.py
@@ -11,53 +11,45 @@
 # citizens (10)
 # firefighters (11)
 
+import Mapping
 
-current_map = [['\0' for _ in range(550)] for _ in range(100)]
-map = current_map
-predicted_map = [['\0' for _ in range(550)] for _ in range(100)]
 
-def FirePrediction():
+def fire_prediction(Current_map):
+    map = Current_map
     for x in range(550):
         for y in range (100):
             ## See if there is a fire, if the severity is greater than 4, and if the windspeed is greater than 0
-            if get_fire(x, y) == 1 & get_fire(x,y) >= 4 & get_wind(x,y) >= 3:
+            if Mapping.get_fire(x, y) == 1 and Mapping.get_fire(x,y) >= 4 and Mapping.get_wind(x,y) >= 3:
                         ## Find the wind direction & set new fire adjacent to wind direction
-                        match get_direction(x,y):
+                        match Mapping.get_direction(x,y):
                             case 0:
                                 #Check if there is already a fire at position
+                                if(Mapping.get_fire(x, y - 1) == 1):
+                                    Mapping.set_fire(x, y - 1, Mapping.get_fire(x, y - 1) + 1)
                                 if(get_fire(x, y - 1) >= 1):
-                                    new_fire(x, y - 1, get_fire(x, y - 1) + 1)
+                                    Mapping.set_fire(x, y - 1, get_fire(x, y - 1) + 1)
                                 #if not a new fire set a new one
                                 else:
                                     new_fire(x, y - 1, get_fire(x,y)/2)
                             case 1:
-                                if(get_fire(x + 1, y) >= 1):
-                                    new_fire(x + 1, y, get_fire(x + 1, y) + 1)
+                                if(Mapping.get_fire(x + 1, y) == 1):
+                                    new_severity(x + 1, y, get_severity(x + 1, y) + 1)
+                                if(Mapping.get_fire(x + 1, y) >= 1):
+                                    Mapping.set_fire(x + 1, y, Mapping.get_fire(x + 1, y) + 1)
                                 else:
-                                    new_fire(x + 1, y, get_fire(x,y)/2)
+                                    Mapping.set_fire(x + 1, y, get_fire(x,y)/2)
                             case 2:
                                 if(get_fire(x, y + 1) >= 1):
-                                    new_fire(x, y + 1, get_fire(x, y - 1) + 1)
+                                    Mapping.set_fire(x, y + 1, get_fire(x, y - 1) + 1)
                                 else:
-                                    new_fire(x, y + 1, get_fire(x,y)/2)
+                                    Mapping.set_fire(x, y + 1, get_fire(x,y)/2)
                             case 3:
+                                if(Mapping.get_fire(x - 1, y) == 1):
+                                    new_severity(x - 1, y, get_severity(x - 1, y) + 1)
                                 if(get_fire(x - 1, y) >= 1):
-                                    new_fire(x - 1, y, get_fire(x - 1, y) + 1)
+                                    Mapping.set_fire(x - 1, y, get_fire(x - 1, y) + 1)
                                 else:
-                                    new_fire(x - 1, y, get_fire(x,y)/2)
+                                    Mapping.set_fire(x - 1, y, get_fire(x,y)/2)
+                                    
+    Mapping.update_predicted_map(map)
     return
-
-#Getter
-def get_fire(x,y):
-    return current_map[x][y][6]
-def get_wind(x,y):
-    return current_map[x][y][7]
-def get_direction(x,y):
-    return current_map[x][y][8]
-
-#Setter
-def new_fire(x,y,n):
-    map[x][y][6] = n
-    return
-
-set(predicted_map) = map
