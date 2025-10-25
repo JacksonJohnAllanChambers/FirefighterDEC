7239385d2a83bf1b6e2114ebf3f199c66a9276a5
LiamLegge <liamlegge53@gmail.com>
2025-10-25 13:40:29 -0300

    Half Drone Search Update

--- DroneSearch.py
+++ DroneSearch.py
@@ -1,2 +1,29 @@
-def Drone_Search():
-    machine.learn
+import Mapping
+import fireClasses
+
+quit = 0
+
+x = fireClasses.drone.x
+y = fireClasses.drone.y
+x_limit = 0
+y_limit = 0
+
+highFire = 0
+
+Drone_map = [['\0' for _ in range(x_limit)] for _ in range(y_limit)]
+
+def Search_Fire():
+    if quit == 1:
+        return
+    while x_limit <= x <= x_limit + 1:
+        while y_limit <= y <= y_limit + 1:
+            fire = Mapping.get_fire(x,y)
+            if fire >= highFire:
+                highFire = (x,y)
+            if highFire != 0:
+                Mapping.move(highFire)
+            else:
+                Search_Fire
+
+def Empty_Search():
+    Mapping.get_info(x,y)
